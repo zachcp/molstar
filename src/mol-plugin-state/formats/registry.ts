e32210f330b5d1012a77f96d80a6826860b0a48d
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-10-13 19:09:23 -0400

    Add return type to auto method (755 errors, 721 fixable, 9 total fixed)

--- src/mol-plugin-state/formats/registry.ts
+++ src/mol-plugin-state/formats/registry.ts
@@ -5,100 +5,120 @@
  * @author David Sehnal <david.sehnal@gmail.com>
  */
 
-import type { FileNameInfo } from '../../mol-util/file-info.ts';
-import type { PluginStateObject } from '../objects.ts';
-import type { DataFormatProvider } from './provider.ts';
-import { BuiltInTrajectoryFormats } from './trajectory.ts';
-import { BuiltInVolumeFormats } from './volume.ts';
-import { BuiltInShapeFormats } from './shape.ts';
-import { BuiltInTopologyFormats } from './topology.ts';
-import { BuiltInCoordinatesFormats } from './coordinates.ts';
+import type { FileNameInfo } from "../../mol-util/file-info.ts";
+import type { PluginStateObject } from "../objects.ts";
+import type { DataFormatProvider } from "./provider.ts";
+import { BuiltInTrajectoryFormats } from "./trajectory.ts";
+import { BuiltInVolumeFormats } from "./volume.ts";
+import { BuiltInShapeFormats } from "./shape.ts";
+import { BuiltInTopologyFormats } from "./topology.ts";
+import { BuiltInCoordinatesFormats } from "./coordinates.ts";
 
 export class DataFormatRegistry {
-    private _list: { name: string, provider: DataFormatProvider }[] = [];
-    private _map = new Map<string, DataFormatProvider>();
-    private _extensions: Set<string> | undefined = undefined;
-    private _binaryExtensions: Set<string> | undefined = undefined;
-    private _options: [name: string, label: string, category: string][] | undefined = undefined;
+  private _list: { name: string; provider: DataFormatProvider }[] = [];
+  private _map = new Map<string, DataFormatProvider>();
+  private _extensions: Set<string> | undefined = undefined;
+  private _binaryExtensions: Set<string> | undefined = undefined;
+  private _options:
+    | [name: string, label: string, category: string][]
+    | undefined = undefined;
 
-    get types(): [name: string, label: string][] {
-        return this._list.map(e => [e.name, e.provider.label] as [name: string, label: string]);
+  get types(): [name: string, label: string][] {
+    return this._list.map(
+      (e) => [e.name, e.provider.label] as [name: string, label: string],
+    );
+  }
+
+  get extensions(): Set<string> {
+    if (this._extensions) return this._extensions;
+    const extensions = new Set<string>();
+    this._list.forEach(({ provider }) => {
+      provider.stringExtensions?.forEach((ext) => extensions.add(ext));
+      provider.binaryExtensions?.forEach((ext) => extensions.add(ext));
+    });
+    this._extensions = extensions;
+    return extensions;
+  }
+
+  get binaryExtensions(): Set<string> {
+    if (this._binaryExtensions) return this._binaryExtensions;
+    const binaryExtensions = new Set<string>();
+    this._list.forEach(({ provider }) =>
+      provider.binaryExtensions?.forEach((ext) => binaryExtensions.add(ext)),
+    );
+    this._binaryExtensions = binaryExtensions;
+    return binaryExtensions;
+  }
+
+  get options(): [name: string, label: string, category: string][] {
+    if (this._options) return this._options;
+    const options: [name: string, label: string, category: string][] = [];
+    this._list.forEach(({ name, provider }) =>
+      options.push([name, provider.label, provider.category || ""]),
+    );
+    this._options = options;
+    return options;
+  }
+
+  constructor() {
+    for (const [id, p] of BuiltInVolumeFormats) this.add(id, p);
+    for (const [id, p] of BuiltInTopologyFormats) this.add(id, p);
+    for (const [id, p] of BuiltInCoordinatesFormats) this.add(id, p);
+    for (const [id, p] of BuiltInShapeFormats) this.add(id, p);
+    for (const [id, p] of BuiltInTrajectoryFormats) this.add(id, p);
+  }
+
+  private _clear() {
+    this._extensions = undefined;
+    this._binaryExtensions = undefined;
+    this._options = undefined;
+  }
+
+  add(name: string, provider: DataFormatProvider) {
+    this._clear();
+    this._list.push({ name, provider });
+    this._map.set(name, provider);
+  }
+
+  remove(name: string) {
+    this._clear();
+    this._list.splice(
+      this._list.findIndex((e) => e.name === name),
+      1,
+    );
+    this._map.delete(name);
+  }
+
+  auto(
+    info: FileNameInfo,
+    dataStateObject:
+      | PluginStateObject.Data.Binary
+      | PluginStateObject.Data.String,
+  ): DataFormatProvider | undefined {
+    for (let i = 0, il = this.list.length; i < il; ++i) {
+      const p = this._list[i].provider;
+
+      let hasExt = false;
+      if (p.binaryExtensions?.includes(info.ext)) hasExt = true;
+      else if (p.stringExtensions?.includes(info.ext)) hasExt = true;
+      if (
+        hasExt &&
+        (!p.isApplicable || p.isApplicable(info, dataStateObject.data))
+      )
+        return p;
     }
+    return;
+  }
 
-    get extensions(): Set<string> {
-        if (this._extensions) return this._extensions;
-        const extensions = new Set<string>();
-        this._list.forEach(({ provider }) => {
-            provider.stringExtensions?.forEach(ext => extensions.add(ext));
-            provider.binaryExtensions?.forEach(ext => extensions.add(ext));
-        });
-        this._extensions = extensions;
-        return extensions;
+  get(name: string): DataFormatProvider | undefined {
+    if (this._map.has(name)) {
+      return this._map.get(name)!;
+    } else {
+      throw new Error(`unknown data format name '${name}'`);
     }
+  }
 
-    get binaryExtensions(): Set<string> {
-        if (this._binaryExtensions) return this._binaryExtensions;
-        const binaryExtensions = new Set<string>();
-        this._list.forEach(({ provider }) => provider.binaryExtensions?.forEach(ext => binaryExtensions.add(ext)));
-        this._binaryExtensions = binaryExtensions;
-        return binaryExtensions;
-    }
-
-    get options(): [name: string, label: string, category: string][] {
-        if (this._options) return this._options;
-        const options: [name: string, label: string, category: string][] = [];
-        this._list.forEach(({ name, provider }) => options.push([name, provider.label, provider.category || '']));
-        this._options = options;
-        return options;
-    }
-
-    constructor() {
-        for (const [id, p] of BuiltInVolumeFormats) this.add(id, p);
-        for (const [id, p] of BuiltInTopologyFormats) this.add(id, p);
-        for (const [id, p] of BuiltInCoordinatesFormats) this.add(id, p);
-        for (const [id, p] of BuiltInShapeFormats) this.add(id, p);
-        for (const [id, p] of BuiltInTrajectoryFormats) this.add(id, p);
-    };
-
-    private _clear() {
-        this._extensions = undefined;
-        this._binaryExtensions = undefined;
-        this._options = undefined;
-    }
-
-    add(name: string, provider: DataFormatProvider) {
-        this._clear();
-        this._list.push({ name, provider });
-        this._map.set(name, provider);
-    }
-
-    remove(name: string) {
-        this._clear();
-        this._list.splice(this._list.findIndex(e => e.name === name), 1);
-        this._map.delete(name);
-    }
-
-    auto(info: FileNameInfo, dataStateObject: PluginStateObject.Data.Binary | PluginStateObject.Data.String) {
-        for (let i = 0, il = this.list.length; i < il; ++i) {
-            const p = this._list[i].provider;
-
-            let hasExt = false;
-            if (p.binaryExtensions?.includes(info.ext)) hasExt = true;
-            else if (p.stringExtensions?.includes(info.ext)) hasExt = true;
-            if (hasExt && (!p.isApplicable || p.isApplicable(info, dataStateObject.data))) return p;
-        }
-        return;
-    }
-
-    get(name: string): DataFormatProvider | undefined {
-        if (this._map.has(name)) {
-            return this._map.get(name)!;
-        } else {
-            throw new Error(`unknown data format name '${name}'`);
-        }
-    }
-
-    get list(): { name: string, provider: DataFormatProvider }[] {
-        return this._list;
-    }
+  get list(): { name: string; provider: DataFormatProvider }[] {
+    return this._list;
+  }
 }
