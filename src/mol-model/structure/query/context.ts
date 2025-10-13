fce6e9f03ea4472e0406f2f6aa6917a42d76cbdd
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-10-13 19:07:44 -0400

    Add return types to methods (756 errors, 722 fixable, 8 total fixed)

--- src/mol-model/structure/query/context.ts
+++ src/mol-model/structure/query/context.ts
@@ -5,156 +5,171 @@
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  */
 
-import { type Structure, StructureElement, type Unit } from '../structure.ts';
-import { now } from '../../../mol-util/now.ts';
-import { BondType } from '../model/types.ts';
-import type { StructureSelection } from './selection.ts';
-import { defaultBondTest } from './queries/internal.ts';
+import { type Structure, StructureElement, type Unit } from "../structure.ts";
+import { now } from "../../../mol-util/now.ts";
+import { BondType } from "../model/types.ts";
+import type { StructureSelection } from "./selection.ts";
+import { defaultBondTest } from "./queries/internal.ts";
 
 export interface QueryContextView {
-    readonly element: Readonly<StructureElement.Location>;
-    readonly currentStructure: Structure;
+  readonly element: Readonly<StructureElement.Location>;
+  readonly currentStructure: Structure;
 }
 
 export class QueryContext implements QueryContextView {
-    private currentElementStack: StructureElement.Location[] = [];
-    private currentAtomicBondStack: QueryContextBondInfo<Unit.Atomic>[] = [];
-    private currentStructureStack: Structure[] = [];
-    private inputStructureStack: Structure[] = [];
+  private currentElementStack: StructureElement.Location[] = [];
+  private currentAtomicBondStack: QueryContextBondInfo<Unit.Atomic>[] = [];
+  private currentStructureStack: Structure[] = [];
+  private inputStructureStack: Structure[] = [];
 
-    private timeCreated = now();
+  private timeCreated = now();
 
-    readonly timeoutMs: number;
-    readonly inputStructure: Structure;
+  readonly timeoutMs: number;
+  readonly inputStructure: Structure;
 
-    /** Current element */
-    readonly element: StructureElement.Location = StructureElement.Location.create(void 0);
-    currentStructure: Structure = void 0 as any;
+  /** Current element */
+  readonly element: StructureElement.Location =
+    StructureElement.Location.create(void 0);
+  currentStructure: Structure = void 0 as any;
 
-    /** Current bond between atoms */
-    readonly atomicBond = new QueryContextBondInfo<Unit.Atomic>();
+  /** Current bond between atoms */
+  readonly atomicBond = new QueryContextBondInfo<Unit.Atomic>();
 
-    /** Supply this from the outside. Used by the internal.generator.current symbol */
-    currentSelection: StructureSelection | undefined = void 0;
+  /** Supply this from the outside. Used by the internal.generator.current symbol */
+  currentSelection: StructureSelection | undefined = void 0;
 
-    pushCurrentElement(): StructureElement.Location {
-        this.currentElementStack[this.currentElementStack.length] = this.element;
-        (this.element as StructureElement.Location) = StructureElement.Location.create(void 0);
-        return this.element;
+  pushCurrentElement(): StructureElement.Location {
+    this.currentElementStack[this.currentElementStack.length] = this.element;
+    (this.element as StructureElement.Location) =
+      StructureElement.Location.create(void 0);
+    return this.element;
+  }
+
+  popCurrentElement() {
+    (this.element as StructureElement.Location) =
+      this.currentElementStack.pop()!;
+  }
+
+  pushCurrentBond(): QueryContextBondInfo<Unit.Atomic> {
+    if (this.atomicBond) this.currentAtomicBondStack.push(this.atomicBond);
+    (this.atomicBond as QueryContextBondInfo<Unit.Atomic>) =
+      new QueryContextBondInfo();
+    return this.atomicBond;
+  }
+
+  popCurrentBond() {
+    if (this.currentAtomicBondStack.length > 0) {
+      (this.atomicBond as QueryContextBondInfo<Unit.Atomic>) =
+        this.currentAtomicBondStack.pop()!;
+    } else {
+      (this.atomicBond as any) = void 0;
     }
+  }
 
-    popCurrentElement() {
-        (this.element as StructureElement.Location) = this.currentElementStack.pop()!;
-    }
+  pushCurrentStructure() {
+    if (this.currentStructure)
+      this.currentStructureStack.push(this.currentStructure);
+  }
 
-    pushCurrentBond() {
-        if (this.atomicBond) this.currentAtomicBondStack.push(this.atomicBond);
-        (this.atomicBond as QueryContextBondInfo<Unit.Atomic>) = new QueryContextBondInfo();
-        return this.atomicBond;
-    }
+  popCurrentStructure() {
+    if (this.currentStructureStack.length)
+      (this.currentStructure as Structure) = this.currentStructureStack.pop()!;
+    else (this.currentStructure as Structure) = void 0 as any;
+  }
 
-    popCurrentBond() {
-        if (this.currentAtomicBondStack.length > 0) {
-            (this.atomicBond as QueryContextBondInfo<Unit.Atomic>) = this.currentAtomicBondStack.pop()!;
-        } else {
-            (this.atomicBond as any) = void 0;
-        }
-    }
+  pushInputStructure(structure: Structure) {
+    this.inputStructureStack.push(this.inputStructure);
+    (this.inputStructure as any) = structure;
+  }
 
-    pushCurrentStructure() {
-        if (this.currentStructure) this.currentStructureStack.push(this.currentStructure);
-    }
+  popInputStructure() {
+    if (this.inputStructureStack.length === 0)
+      throw new Error("Must push before pop.");
+    (this.inputStructure as any) = this.inputStructureStack.pop();
+  }
 
-    popCurrentStructure() {
-        if (this.currentStructureStack.length) (this.currentStructure as Structure) = this.currentStructureStack.pop()!;
-        else (this.currentStructure as Structure) = void 0 as any;
+  throwIfTimedOut() {
+    if (this.timeoutMs === 0) return;
+    if (now() - this.timeCreated > this.timeoutMs) {
+      throw new Error(
+        `The query took too long to execute (> ${this.timeoutMs / 1000}s).`,
+      );
     }
+  }
 
-    pushInputStructure(structure: Structure) {
-        this.inputStructureStack.push(this.inputStructure);
-        (this.inputStructure as any) = structure;
-    }
+  tryGetCurrentSelection() {
+    if (!this.currentSelection)
+      throw new Error("The current selection is not assigned.");
+    return this.currentSelection;
+  }
 
-    popInputStructure() {
-        if (this.inputStructureStack.length === 0) throw new Error('Must push before pop.');
-        (this.inputStructure as any) = this.inputStructureStack.pop();
-    }
-
-    throwIfTimedOut() {
-        if (this.timeoutMs === 0) return;
-        if (now() - this.timeCreated > this.timeoutMs) {
-            throw new Error(`The query took too long to execute (> ${this.timeoutMs / 1000}s).`);
-        }
-    }
-
-    tryGetCurrentSelection() {
-        if (!this.currentSelection) throw new Error('The current selection is not assigned.');
-        return this.currentSelection;
-    }
-
-    constructor(structure: Structure, options?: QueryContextOptions) {
-        this.inputStructure = structure;
-        this.timeoutMs = (options && options.timeoutMs) || 0;
-        this.currentSelection = options && options.currentSelection;
-    }
+  constructor(structure: Structure, options?: QueryContextOptions) {
+    this.inputStructure = structure;
+    this.timeoutMs = (options && options.timeoutMs) || 0;
+    this.currentSelection = options && options.currentSelection;
+  }
 }
 
 export interface QueryContextOptions {
-    timeoutMs?: number,
-    currentSelection?: StructureSelection
+  timeoutMs?: number;
+  currentSelection?: StructureSelection;
 }
 
-export interface QueryPredicate { (ctx: QueryContext): boolean }
-export interface QueryFn<T = any> { (ctx: QueryContext): T }
+export interface QueryPredicate {
+  (ctx: QueryContext): boolean;
+}
+export interface QueryFn<T = any> {
+  (ctx: QueryContext): T;
+}
 
 class QueryContextBondInfo<U extends Unit = Unit> {
-    a: StructureElement.Location<U> = StructureElement.Location.create(void 0);
-    aIndex: StructureElement.UnitIndex = 0 as StructureElement.UnitIndex;
-    b: StructureElement.Location<U> = StructureElement.Location.create(void 0);
-    bIndex: StructureElement.UnitIndex = 0 as StructureElement.UnitIndex;
-    type: BondType = BondType.Flag.None;
-    order: number = 0;
-    key: number = -1;
+  a: StructureElement.Location<U> = StructureElement.Location.create(void 0);
+  aIndex: StructureElement.UnitIndex = 0 as StructureElement.UnitIndex;
+  b: StructureElement.Location<U> = StructureElement.Location.create(void 0);
+  bIndex: StructureElement.UnitIndex = 0 as StructureElement.UnitIndex;
+  type: BondType = BondType.Flag.None;
+  order: number = 0;
+  key: number = -1;
 
-    private testFn: QueryPredicate = defaultBondTest;
+  private testFn: QueryPredicate = defaultBondTest;
 
-    setStructure(s: Structure) {
-        this.a.structure = s;
-        this.b.structure = s;
+  setStructure(s: Structure) {
+    this.a.structure = s;
+    this.b.structure = s;
+  }
+
+  setTestFn(fn?: QueryPredicate) {
+    this.testFn = fn || defaultBondTest;
+  }
+
+  test(ctx: QueryContext, trySwap: boolean) {
+    if (this.testFn(ctx)) return true;
+    if (trySwap) {
+      this.swap();
+      return this.testFn(ctx);
     }
+    return false;
+  }
 
-    setTestFn(fn?: QueryPredicate) {
-        this.testFn = fn || defaultBondTest;
-    }
+  private swap() {
+    // const sA = this.a.structure;
+    // this.a.structure = this.b.structure;
+    // this.b.structure = sA;
 
-    test(ctx: QueryContext, trySwap: boolean) {
-        if (this.testFn(ctx)) return true;
-        if (trySwap) {
-            this.swap();
-            return this.testFn(ctx);
-        }
-        return false;
-    }
+    const idxA = this.aIndex;
+    this.aIndex = this.bIndex;
+    this.bIndex = idxA;
 
-    private swap() {
-        // const sA = this.a.structure;
-        // this.a.structure = this.b.structure;
-        // this.b.structure = sA;
+    const unitA = this.a.unit;
+    this.a.unit = this.b.unit;
+    this.b.unit = unitA;
 
-        const idxA = this.aIndex;
-        this.aIndex = this.bIndex;
-        this.bIndex = idxA;
+    const eA = this.a.element;
+    this.a.element = this.b.element;
+    this.b.element = eA;
+  }
 
-        const unitA = this.a.unit;
-        this.a.unit = this.b.unit;
-        this.b.unit = unitA;
-
-        const eA = this.a.element;
-        this.a.element = this.b.element;
-        this.b.element = eA;
-    }
-
-    get length(): number {
-        return StructureElement.Location.distance(this.a, this.b);
-    }
+  get length(): number {
+    return StructureElement.Location.distance(this.a, this.b);
+  }
 }
