08df61e92fb1ab6f07359a111b25773b16ee782d
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-10-13 20:12:28 -0400

    Add explicit type to readonly events in substructure-parent-helper.ts (660→659)

--- src/mol-plugin/util/substructure-parent-helper.ts
+++ src/mol-plugin/util/substructure-parent-helper.ts
@@ -4,109 +4,147 @@
  * @author David Sehnal <david.sehnal@gmail.com>
  */
 
-import type { Structure } from '../../mol-model/structure.ts';
-import { PluginStateObject } from '../../mol-plugin-state/objects.ts';
-import { type State, type StateObject, type StateObjectCell, StateSelection } from '../../mol-state/index.ts';
-import type { PluginContext } from '../context.ts';
-import { RxEventHelper } from '../../mol-util/rx-event-helper.ts';
+import type { Structure } from "../../mol-model/structure.ts";
+import { PluginStateObject } from "../../mol-plugin-state/objects.ts";
+import {
+  type State,
+  type StateObject,
+  type StateObjectCell,
+  StateSelection,
+} from "../../mol-state/index.ts";
+import type { PluginContext } from "../context.ts";
+import { RxEventHelper } from "../../mol-util/rx-event-helper.ts";
+import type { Subject } from "rxjs";
 
 export { SubstructureParentHelper };
 
 class SubstructureParentHelper {
-    private ev = RxEventHelper.create();
+  private ev = RxEventHelper.create();
 
-    readonly events = {
-        updated: this.ev<{ ref: string, oldObj: PluginStateObject.Molecule.Structure | undefined, obj: PluginStateObject.Molecule.Structure }>(),
-        removed: this.ev<{ ref: string, obj: PluginStateObject.Molecule.Structure | undefined }>(),
-    };
+  readonly events: {
+    updated: Subject<{
+      ref: string;
+      oldObj: PluginStateObject.Molecule.Structure | undefined;
+      obj: PluginStateObject.Molecule.Structure;
+    }>;
+    removed: Subject<{
+      ref: string;
+      obj: PluginStateObject.Molecule.Structure | undefined;
+    }>;
+  } = {
+    updated: this.ev<{
+      ref: string;
+      oldObj: PluginStateObject.Molecule.Structure | undefined;
+      obj: PluginStateObject.Molecule.Structure;
+    }>(),
+    removed: this.ev<{
+      ref: string;
+      obj: PluginStateObject.Molecule.Structure | undefined;
+    }>(),
+  };
 
-    // private decorators = new Map<string, string[]>();
-    private root = new Map<Structure, { ref: string, count: number }>();
-    private tracked = new Map<string, Structure>();
+  // private decorators = new Map<string, string[]>();
+  private root = new Map<Structure, { ref: string; count: number }>();
+  private tracked = new Map<string, Structure>();
 
-    private getDecorator(root: string): string {
-        const tree = this.plugin.state.data.tree;
-        const children = tree.children.get(root);
-        if (children.size !== 1) return root;
-        const child = children.first();
-        if (child && tree.transforms.get(child).transformer.definition.isDecorator) {
-            return this.getDecorator(child);
-        }
-        return root;
+  private getDecorator(root: string): string {
+    const tree = this.plugin.state.data.tree;
+    const children = tree.children.get(root);
+    if (children.size !== 1) return root;
+    const child = children.first();
+    if (
+      child &&
+      tree.transforms.get(child).transformer.definition.isDecorator
+    ) {
+      return this.getDecorator(child);
     }
+    return root;
+  }
 
-    /** Returns the root node of given structure if existing, takes decorators into account */
-    get(s: Structure, ignoreDecorators = false): StateObjectCell<PluginStateObject.Molecule.Structure> | undefined {
-        const r = this.root.get(s);
-        if (!r) return;
-        if (ignoreDecorators) return this.plugin.state.data.cells.get(r.ref);
-        return this.plugin.state.data.cells.get(this.getDecorator(r.ref));
+  /** Returns the root node of given structure if existing, takes decorators into account */
+  get(
+    s: Structure,
+    ignoreDecorators = false,
+  ): StateObjectCell<PluginStateObject.Molecule.Structure> | undefined {
+    const r = this.root.get(s);
+    if (!r) return;
+    if (ignoreDecorators) return this.plugin.state.data.cells.get(r.ref);
+    return this.plugin.state.data.cells.get(this.getDecorator(r.ref));
+  }
+
+  private addMapping(state: State, ref: string, obj: StateObject) {
+    if (!PluginStateObject.Molecule.Structure.is(obj)) return false;
+
+    this.tracked.set(ref, obj.data);
+
+    // if the structure is already present in the tree, do not rewrite the root.
+    if (this.root.has(obj.data)) {
+      const e = this.root.get(obj.data)!;
+      e.count++;
+    } else {
+      const parent = state.select(
+        StateSelection.Generators.byRef(ref).rootOfType([
+          PluginStateObject.Molecule.Structure,
+        ]),
+      )[0];
+
+      if (!parent) {
+        this.root.set(obj.data, { ref, count: 1 });
+      } else {
+        this.root.set(obj.data, { ref: parent.transform.ref, count: 1 });
+      }
     }
+    return true;
+  }
 
-    private addMapping(state: State, ref: string, obj: StateObject) {
-        if (!PluginStateObject.Molecule.Structure.is(obj)) return false;
+  private removeMapping(ref: string) {
+    if (!this.tracked.has(ref)) return false;
 
-        this.tracked.set(ref, obj.data);
+    const s = this.tracked.get(ref)!;
+    this.tracked.delete(ref);
 
-        // if the structure is already present in the tree, do not rewrite the root.
-        if (this.root.has(obj.data)) {
-            const e = this.root.get(obj.data)!;
-            e.count++;
-        } else {
-            const parent = state.select(StateSelection.Generators.byRef(ref).rootOfType([PluginStateObject.Molecule.Structure]))[0];
+    const root = this.root.get(s)!;
 
-            if (!parent) {
-                this.root.set(obj.data, { ref, count: 1 });
-            } else {
-                this.root.set(obj.data, { ref: parent.transform.ref, count: 1 });
-            }
-        }
-        return true;
+    if (root.count > 1) {
+      root.count--;
+    } else {
+      this.root.delete(s);
     }
+    return true;
+  }
 
-    private removeMapping(ref: string) {
-        if (!this.tracked.has(ref)) return false;
+  private updateMapping(
+    state: State,
+    ref: string,
+    oldObj: StateObject | undefined,
+    obj: StateObject,
+  ) {
+    if (!PluginStateObject.Molecule.Structure.is(obj)) return false;
 
-        const s = this.tracked.get(ref)!;
-        this.tracked.delete(ref);
+    this.removeMapping(ref);
+    this.addMapping(state, ref, obj);
+    return true;
+  }
 
-        const root = this.root.get(s)!;
+  dispose() {
+    this.ev.dispose();
+  }
 
-        if (root.count > 1) {
-            root.count--;
-        } else {
-            this.root.delete(s);
-        }
-        return true;
-    }
+  constructor(private plugin: PluginContext) {
+    plugin.state.data.events.object.created.subscribe((e) => {
+      this.addMapping(e.state, e.ref, e.obj);
+    });
 
-    private updateMapping(state: State, ref: string, oldObj: StateObject | undefined, obj: StateObject) {
-        if (!PluginStateObject.Molecule.Structure.is(obj)) return false;
+    plugin.state.data.events.object.removed.subscribe((e) => {
+      if (this.removeMapping(e.ref)) {
+        this.events.removed.next({ ref: e.ref, obj: e.obj });
+      }
+    });
 
-        this.removeMapping(ref);
-        this.addMapping(state, ref, obj);
-        return true;
-    }
-
-    dispose() {
-        this.ev.dispose();
-    }
-
-    constructor(private plugin: PluginContext) {
-        plugin.state.data.events.object.created.subscribe(e => {
-            this.addMapping(e.state, e.ref, e.obj);
-        });
-
-        plugin.state.data.events.object.removed.subscribe(e => {
-            if (this.removeMapping(e.ref)) {
-                this.events.removed.next({ ref: e.ref, obj: e.obj });
-            }
-        });
-
-        plugin.state.data.events.object.updated.subscribe(e => {
-            if (this.updateMapping(e.state, e.ref, e.oldObj, e.obj)) {
-                this.events.updated.next({ ref: e.ref, oldObj: e.oldObj, obj: e.obj });
-            }
-        });
-    }
+    plugin.state.data.events.object.updated.subscribe((e) => {
+      if (this.updateMapping(e.state, e.ref, e.oldObj, e.obj)) {
+        this.events.updated.next({ ref: e.ref, oldObj: e.oldObj, obj: e.obj });
+      }
+    });
+  }
 }
