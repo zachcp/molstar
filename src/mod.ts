1c7c2d9e8cf8c283ed51bb15060ac2180f762f48
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-11-09 17:15:02 -0500

    add mod and export all

--- src/mod.ts
+++ src/mod.ts
@@ -6,14 +6,61 @@
  */
 
 // Core modules
-export * from './mol-data/index.ts';
-export * from './mol-state/index.ts';
-export * from './mol-task/index.ts';
-export * from './mol-util/index.ts';
+export * from "./mol-data/index.ts";
+export * from "./mol-state/index.ts";
+export * from "./mol-task/index.ts";
+export * from "./mol-util/index.ts";
 
 // Plugin modules
-export * from './mol-plugin/index.ts';
-export * from './mol-plugin-ui/index.ts';
+export * from "./mol-plugin/index.ts";
+export * from "./mol-plugin-ui/index.ts";
+export * from "./mol-plugin-state/index.ts";
 
 // Canvas
-export * from './mol-canvas3d/index.ts';
+export * from "./mol-canvas3d/index.ts";
+
+// Model and structure
+export * from "./mol-model/index.ts";
+export * from "./mol-model-formats/index.ts";
+export * from "./mol-model-props/index.ts";
+
+// Geometry and representation
+export * from "./mol-geo/index.ts";
+export * from "./mol-repr/index.ts";
+
+// GL and rendering
+export * from "./mol-gl/index.ts";
+
+// Math
+export * from "./mol-math/index.ts";
+
+// IO
+export * from "./mol-io/index.ts";
+
+// Script
+export * from "./mol-script/index.ts";
+
+// Theme
+export * from "./mol-theme/index.ts";
+
+// Extensions
+export * from "./extensions/alpha-orbitals/index.ts";
+export * from "./extensions/anvil/index.ts";
+export * from "./extensions/assembly-symmetry/index.ts";
+export * from "./extensions/backgrounds/index.ts";
+export * from "./extensions/dnatco/index.ts";
+export * from "./extensions/g3d/index.ts";
+export * from "./extensions/geo-export/index.ts";
+export * from "./extensions/interactions/index.ts";
+export * from "./extensions/json-cif/index.ts";
+export * from "./extensions/meshes/index.ts";
+export * from "./extensions/model-archive/index.ts";
+export * from "./extensions/model-export/index.ts";
+export * from "./extensions/mp4-export/index.ts";
+export * from "./extensions/mvs/index.ts";
+export * from "./extensions/pdbe/index.ts";
+export * from "./extensions/rcsb/index.ts";
+export * from "./extensions/sb-ncbr/index.ts";
+export * from "./extensions/volumes-and-segmentations/index.ts";
+export * from "./extensions/wwpdb/index.ts";
+export * from "./extensions/zenodo/index.ts";
