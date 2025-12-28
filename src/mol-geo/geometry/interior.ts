13bc635b73d1e4df902d77ae6223675f97bd029c
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-12-28 15:09:06 -0500

    fix: add explicit return type to getInteriorParam in interior.ts

--- src/mol-geo/geometry/interior.ts
+++ src/mol-geo/geometry/interior.ts
@@ -4,36 +4,48 @@
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  */
 
-import { Vec4 } from '../../mol-math/linear-algebra/3d/vec4.ts';
-import { Color } from '../../mol-util/color/color.ts';
-import { Material } from '../../mol-util/material.ts';
-import { ParamDefinition as PD } from '../../mol-util/param-definition.ts';
+import { Vec4 } from "../../mol-math/linear-algebra/3d/vec4.ts";
+import { Color } from "../../mol-util/color/color.ts";
+import { Material } from "../../mol-util/material.ts";
+import { ParamDefinition as PD } from "../../mol-util/param-definition.ts";
 
-export function getInteriorParam() {
-    return PD.Group({
-        color: PD.Color(Color.fromRgb(76, 76, 76)),
-        colorStrength: PD.Numeric(1, { min: 0, max: 1, step: 0.01 }),
-        substance: Material.getParam(),
-        substanceStrength: PD.Numeric(1, { min: 0, max: 1, step: 0.01 }),
-    });
+export function getInteriorParam(): PD.Group<
+  PD.Normalize<{
+    color: PD.Color;
+    colorStrength: PD.Numeric;
+    substance: ReturnType<typeof Material.getParam>;
+    substanceStrength: PD.Numeric;
+  }>
+> {
+  return PD.Group({
+    color: PD.Color(Color.fromRgb(76, 76, 76)),
+    colorStrength: PD.Numeric(1, { min: 0, max: 1, step: 0.01 }),
+    substance: Material.getParam(),
+    substanceStrength: PD.Numeric(1, { min: 0, max: 1, step: 0.01 }),
+  });
 }
-export type InteriorProp = ReturnType<typeof getInteriorParam>['defaultValue'];
+export type InteriorProp = ReturnType<typeof getInteriorParam>["defaultValue"];
 
-export function areInteriorPropsEquals(a: InteriorProp, b: InteriorProp): boolean {
-    return a.color === b.color
-        && a.colorStrength === b.colorStrength
-        && Material.areEqual(a.substance, b.substance)
-        && a.substanceStrength === b.substanceStrength;
+export function areInteriorPropsEquals(
+  a: InteriorProp,
+  b: InteriorProp,
+): boolean {
+  return (
+    a.color === b.color &&
+    a.colorStrength === b.colorStrength &&
+    Material.areEqual(a.substance, b.substance) &&
+    a.substanceStrength === b.substanceStrength
+  );
 }
 
 export function getInteriorColor(props: InteriorProp, out: Vec4): Vec4 {
-    Color.toArrayNormalized(props.color, out, 0);
-    out[3] = props.colorStrength;
-    return out;
+  Color.toArrayNormalized(props.color, out, 0);
+  out[3] = props.colorStrength;
+  return out;
 }
 
 export function getInteriorSubstance(props: InteriorProp, out: Vec4): Vec4 {
-    Material.toArrayNormalized(props.substance, out, 0);
-    out[3] = props.substanceStrength;
-    return out;
+  Material.toArrayNormalized(props.substance, out, 0);
+  out[3] = props.substanceStrength;
+  return out;
 }
